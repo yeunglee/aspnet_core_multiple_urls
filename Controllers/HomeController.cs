0e27df28f2e9a873b0cd8b04407eb7c0541c1713
Yeung <iamyeunglee@gmail.com>
2020-01-01 14:10:25 +0800

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Text;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SetCookie()
+        {
+            HttpContext.Response.Cookies.Append("test", DateTime.Now.Ticks.ToString());
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult SetSession()
+        {
+            HttpContext.Session.Set("test", Encoding.UTF8.GetBytes(DateTime.Now.Ticks.ToString()));
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
